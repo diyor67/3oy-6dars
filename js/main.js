586ac2cf4a623cc42559add24beaa5c5fe867495
diyor67 <diyorbekkarimov031@gmail.com>
2022-11-28 14:54:18 +0500

    Add js

--- js/main.js
+++ js/main.js
@@ -4,12 +4,17 @@ var forHotel = 250;
 var forMuseum = 120;
 var dolarForSum = 11225;
 var euroForSum = 11686;
-var prayc = prompt("Summangizni kiriting");
+var prayc = +prompt('Summangizni kiriting', 0);
 var dolar = (forTicket + forHotel) * dolarForSum;
 var euro = forMuseum * euroForSum;
 var cost = dolar + euro;
 if (prayc >= cost ) {
   elTitle.textContent = "Oq yo'l Alisher😊";
-} else {
+} 
+else if (isNaN(prayc))
+{
+  return 'Alisher aka son kiriting'
+}
+else {
   elTitle.textContent = "Alisher ozgina sabr qilish kerak bo'lar ekan😔";
 }

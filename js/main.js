e7cfef77679bec5a5b16970b172f6af3eb2fbc09
diyor67 <diyorbekkarimov031@gmail.com>
2022-11-28 17:00:53 +0500

    add error

--- js/main.js
+++ js/main.js
@@ -4,17 +4,14 @@ var forHotel = 250;
 var forMuseum = 120;
 var dolarForSum = 11225;
 var euroForSum = 11686;
-var prayc = +prompt('Summangizni kiriting', 0);
+var prayc = prompt('Summangizni kiriting', 0);
 var dolar = (forTicket + forHotel) * dolarForSum;
 var euro = forMuseum * euroForSum;
 var cost = dolar + euro;
 if (prayc >= cost ) {
   elTitle.textContent = "Oq yo'l Alisher😊";
 } 
-else if (isNaN(prayc))
-{
-  return 'Alisher aka son kiriting'
-}
+
 else {
   elTitle.textContent = "Alisher ozgina sabr qilish kerak bo'lar ekan😔";
 }
